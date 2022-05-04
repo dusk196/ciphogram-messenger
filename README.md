83bc805e2d76653c17fa0b57f54274049a9e8e05
Sayantan Roy <sayantan.roy94@gmail.com>
2022-05-04 18:17:19 +0530

    Added Firebase

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SaufMessenger
+# Sauf Messenger [In-Progress]
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.4.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
